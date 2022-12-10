2b2429c724c66927b15d2be1d5cf9c9d25b1d408
Rahoulkhatri <96057417+Rahoulkhatri@users.noreply.github.com>
2022-12-10 22:41:12 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import LoginScreen from './Screens/Auth/LoginScreen'
+import SplashScreen from './Screens/SplashScreen'
+import HomeScreen from './Screens/Home/HomeScreen'
+import ScrollingScreen from './Screens/Home/ScrollingScreen'
+import Navigation from './Navigation/Navigation'
+import CategroyScreen from './Screens/Home/CategroyScreen'
+
+
+
+
+
+
+const App = () => {
+  return (
+    <Navigation/>
+  )
+}
+
+export default App;
